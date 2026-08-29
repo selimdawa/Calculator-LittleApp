693a890e21143047fca53a14b75220202b154d06
selimdawa <selimdawa@gmail.com>
2026-08-29 06:57:29 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/calculator/view/MainActivity.kt
+++ app/src/main/java/com/littleapp/calculator/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.littleapp.calculator.view
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import com.littleapp.calculator.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,9 +16,15 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
     }
--- app/src/main/java/com/littleapp/calculator/view/SplashActivity.kt
+++ app/src/main/java/com/littleapp/calculator/view/SplashActivity.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.littleapp.calculator.utils.CLASS
 import com.littleapp.calculator.utils.THEME
 import com.littleapp.calculator.utils.VOID
@@ -20,9 +23,15 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         THEME.setThemeOfApp(context)
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding!!.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({ launch() }, time_final.toLong())
     }
